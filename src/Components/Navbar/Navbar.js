be6a908fa8ac37a37502f1151b57235d34a4933c
Sultan <akhtersultan007@gmail.com>
2022-12-31 17:56:56 +0530

    Added Scroll Option

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,7 +1,21 @@
 import React from "react";
 import classes from "./Navbar.module.css";
+// import { Link } from "react-router-dom";
+// import Connect from "../Connect/Connect";
 
 const Navbar = () => {
+  const homeScroll = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  const aboutScroll = () => {
+    window.scrollTo({ top: 600, behavior: "smooth" });
+  };
+  const projectScroll = () => {
+    window.scrollTo({ top: 1200, behavior: "smooth" });
+  };
+  const connectScroll = () => {
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
   return (
     <>
       <div className={classes.nav}>
@@ -10,10 +24,11 @@ const Navbar = () => {
         </div>
         <div className={classes.navItems}>
           <ul>
-            <li>Home</li>
-            <li>Projects</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li onClick={homeScroll}>Home</li>
+            <li onClick={aboutScroll}>About</li>
+
+            <li onClick={projectScroll}>Projects</li>
+            <li onClick={connectScroll}>Contact</li>
           </ul>
         </div>
       </div>
--- src/Components/Navbar/Sidebar.js
+++ src/Components/Navbar/Sidebar.js
@@ -3,18 +3,30 @@ import { elastic as Menu } from "react-burger-menu";
 import "./Sidebar.css";
 
 const Sidebar = (props) => {
+  const homeScroll = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  const aboutScroll = () => {
+    window.scrollTo({ top: 800, behavior: "smooth" });
+  };
+  const projectScroll = () => {
+    window.scrollTo({ top: 1700, behavior: "smooth" });
+  };
+  const connectScroll = () => {
+    window.scrollTo({ top: 2800, behavior: "smooth" });
+  };
   return (
     <Menu right>
-      <a className="menu-item" href="/">
+      <a className="menu-item" onClick={homeScroll}>
         Home
       </a>
-      <a className="menu-item" href="/projects">
-        Projects
-      </a>
-      <a className="menu-item" href="/about">
+      <a className="menu-item" onClick={aboutScroll}>
         About
       </a>
-      <a className="menu-item" href="/contact">
+      <a className="menu-item" onClick={projectScroll}>
+        Projects
+      </a>
+      <a className="menu-item" onClick={connectScroll}>
         Contact
       </a>
     </Menu>
